51d5e1fbbf125e1c9bd803546266c8559b168ca2
Javier Wilson <javier.wilson@gmail.com>
2013-12-07 17:41:09 +0000

    soporte para utf8 con mb_substr

--- application/views/services/share.php
+++ application/views/services/share.php
@@ -10,7 +10,7 @@
 		<p class="text"><?=$content?></p>
 		<div class="share-wrap clearfix">
 			<a class="icon fb" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?=$url?>"><? _e('Compartir en Facebook'); ?></a>
-			<a class="icon tw" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?=$url?>&related=RE_media_&text=<? _e('Esta noticia necesita un re:medio'); ?> - '<?= urlencode(substr($report->title, 0, 40)) ?>...' <?=urlencode($url)?><?= ($report->user->allow_mention_twitter && !empty($report->user->twitter)) ?  _(' enviada por @') . $report->user->twitter  : ' '; ?>&via=RE_media_"><? _e('Compartir en Twitter'); ?></a>
+			<a class="icon tw" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?=$url?>&related=RE_media_&text=<? _e('Esta noticia necesita un re%3Amedio'); ?> - '<?= urlencode(mb_substr($report->title, 0, 40)) ?>...' <?=urlencode($url)?><?= ($report->user->allow_mention_twitter && !empty($report->user->twitter)) ?  _(' enviada por @') . $report->user->twitter  : ' '; ?>&via=RE_media_"><? _e('Compartir en Twitter'); ?></a>
 			<a class="icon gp" target="_blank" href="https://plus.google.com/share?url=<?=$url?>"><? _e('Compartir en Google+'); ?></a>
 		</div>
 	</div>
